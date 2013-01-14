5e56d0350cdecb3a2a6240c323b3ab5af263c82d
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-01-14 22:38:45 +0100

    slides-tng works globally now

--- js/slides-tng.js
+++ js/slides-tng.js
@@ -1,13 +1,25 @@
 $(function() {
-    $('[data-slideshow]').each(function() {
-        var panel = $(this), child = 2, children = panel.children();
-        children.hide().eq(child).show();
-        function change() {
-            children.eq(child).fadeOut('slow', function() {
-                if (++child % children.length == 0) child = 0;
-                children.eq(child).fadeIn('slow');
-            });
-        }
-        setInterval(change, panel.attr('data-slideshow') * 1000);
-    });
+    setInterval(function() {
+        $('[data-slideshow]').each(function() {
+            var panel = $(this), 
+                delay = parseInt(panel.attr('data-slideshow'), 10),
+                children = panel.children(), 
+                data = panel.data('slideshow-state') || {},
+                counter = (data.counter || 0) + 1,
+                change = !(counter % delay);
+            if (!data.init) 
+                data.init = children.hide().eq(0).show() && 1;
+            if (change) { 
+                var child = Math.floor((counter - 1) / delay);
+                if (child >= children.length) counter = child = 0;
+                children.eq(child).fadeOut(1500, function() {
+                    if (++child >= children.length) child = 0;
+                    console.log(child, children.length);
+                    children.eq(child).fadeIn('slow');
+                });
+            }
+            data.counter = counter;
+            panel.data('slideshow-state', data);
+        });
+    }, 1000);
 });
