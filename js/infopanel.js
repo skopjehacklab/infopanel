122f412dc752d04dfee6caeebadb5fb8231e2948
Georgi Stanojevski <glisha@gmail.com>
2014-04-19 19:15:59 +0200

    New infopanel code based on https://github.com/raumzeitlabor/infoscreen

--- js/infopanel.js
+++ js/infopanel.js
@@ -19,14 +19,15 @@ function updateStatus() {
                 }
                 otvoren = secondsToString(otvoren/1000);
                 if ( data['current_value'] == 0 ) {
-                    status = "Затворен";
-                    statustime = "веќе " + otvoren;
+                    $("#status").text("Затворен");
+                    $("#status").attr("class","btn zatvoren");
+                    $("#status-time").text("веќе " + otvoren);
                 } else {
-                    status = "Отворен";
-                    statustime = "веќе " + otvoren;
+                    $("#status").text("Отворен");
+                    $("#status").attr("class","btn otvoren");
+                    $("#status-time").text("веќе " + otvoren);
                 }
-                $("#status").text(status);
-                $("#status-time").text(statustime);
+
                 },
         beforeSend: setHeader
         });
@@ -43,13 +44,75 @@ function secondsToString(seconds){
     return numhours + " часови " + numminutes + " минути ";
 }
 
+function updateTemp() {
+    var url = "https://api.cosm.com/v2/feeds/64655/";
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            data['datastreams'].forEach(function(item) {
+                    $("#"+item['id']).text(item['current_value'])})},
+        beforeSend: setHeader
+        });
+}
+
+function updateNetworkSpeeds() {
+    $.getJSON("http://hacklab.ot.mk/ftp/vnstat/json/average.json?callback=?", function(data) {
+        var TK = data['Telekabel'];
+        var BL = data['Blizoo'];
+        $('#tk_rxkbs').text(TK['rxkbs']);
+        $('#tk_txkbs').text(TK['txkbs']);
+        $('#bl_rxkbs').text(BL['rxkbs']);
+        $('#bl_txkbs').text(BL['txkbs']); 
+    })
+}
+
+
+function shuffle(o) {
+    //http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+function populateTumblr() {
+    imgs = shuffle(tumblr_api_read.posts);
+    imgs.forEach(function(item) {
+        $("#tumblr").append("<img src='" + item['photo-url-250'] + "' />");
+    });
+
+}
+
+function randomTumblr() {
+    $('#tumblr img:gt(0)').hide();
+        
+    $('#tumblr :first-child').fadeOut("slow")
+            .next('img').fadeIn("slow")
+            .end().appendTo('#tumblr');
+}
+
+function updateGraphs() {
+    $('#lan_devices').attr('src','https://api.xively.com/v2/feeds/64676/datastreams/lan_devices.png?width=480&height=200&c=ff33ff&duration=24hours&title=Активни лан уреди&stroke_size=2&show_axis_labels=true&detailed_grid=true&scale=auto&timezone=Skopje#' + new Date().getTime());
+
+    $('hacklab_status').attr('src','https://api.cosm.com/v2/feeds/86779/datastreams/hacklab_status.png?width=480&height=200&colour=ff33ff&duration=1day&title=Хаклаб%20Статус&stroke_size=2&show_axis_labels=true&detailed_grid=true&scale=auto&timezone=Skopje&stroke_size=1#' + new Date().getTime());
+
+}
+
 var key="vqElqXeb7Lu6ZwDElnKQ8XpGMG-SAKxxMHV3YWFoeHE4OD0g";
 $(document).ready(function() {
 
     //Прв пат ажурирај
     updateStatus();
+    updateTemp();
+    updateNetworkSpeeds();
+    populateTumblr();
+
+    //на 10 минути ажурирај колку време е отворен, температура
+    window.setInterval("updateStatus()",1000*60*10);
+    window.setInterval("updateTemp()",1000*60*10);
+    window.setInterval("updateGraphs()",1000*60*10);
     
-    //на 10 минути ажурирај колку време е отворен
-    window.setInterval("updateStatus()",600000);
+    //На 10 сек. врти ги сликите
+    window.setInterval("randomTumblr()",1000*10);
 
 });
