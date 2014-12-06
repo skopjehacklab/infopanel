e5f333617e8716bce590b1457319831e8c8fc414
Georgi Stanojevski <glisha@gmail.com>
2014-12-06 21:42:31 +0100

    Obsolete js and change of domain.

--- js/infopanel.js
+++ js/infopanel.js
@@ -58,7 +58,7 @@ function updateTemp() {
 }
 
 function updateNetworkSpeeds() {
-    $.getJSON("http://hacklab.ot.mk/ftp/vnstat/json/average.json?callback=?", function(data) {
+    $.getJSON("http://hacklab.ie.mk/ftp/vnstat/json/average.json?callback=?", function(data) {
         var TK = data['Telekabel'];
         var BL = data['Blizoo'];
         $('#tk_rxkbs').text(TK['rxkbs']);
